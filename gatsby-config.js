61aa729ad3a76cde28fb21e27685c6dc01d9b995
Hugo Méndez <hello@hugomendez.dev>
2021-10-31 19:32:20 -0600

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,12 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
